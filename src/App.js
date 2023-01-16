95ee8221fc48e1d8ee04fba2c556666353e598c1
Johann von Tiesenhausen <johannvont@gmail.com>
2023-01-16 00:47:07 -0500

    websocket/server updates.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import './App.css';
-import { useEffect, useRef, useState } from 'react';
-import './App.css';
+import { useEffect, useRef, useState } from 'react'
+import Cookies from 'universal-cookie'
+
+import './App.css'
 import Board from './components/Board'
 import EnemyBoard from './components/EnemyBoard'
-import { useCookies } from 'react-cookie';
-import generateBoard from './helpers/generateBoard';
-import Customization from './components/Customization';
-import Endofgame from './components/Endofgame';
+import generateBoard from './helpers/generateBoard'
+import Customization from './components/Customization'
+// import Endofgame from './components/Endofgame'
 import styles from './styles/App.module.css'
-import Dashboard from './components/Dashboard';
-import useOrangeMan from './characters/useOrangeMan';
-import useLineMan from './characters/useLineMan';
-let randomstring = require("randomstring");
+import Dashboard from './components/Dashboard'
+import useOrangeMan from './characters/useOrangeMan'
+import useLineMan from './characters/useLineMan'
 
+
+const cookies = new Cookies()
 
 function App() {
-
 
   let { bluffing, setBluffing, OrangeManUI } = useOrangeMan()
   let { setLastShots, LineManUI, shootLine } = useLineMan()
 
-
-  const socket = useRef(null);
-  const [cookies, setCookie, removeCookie] = useCookies(['user']);
+  const socket = useRef(null)
 
   const [gameProgress, setGameProgress] = useState('preplacement')
   const [boardState, setBoardState] = useState(generateBoard(true, true))
@@ -33,72 +31,77 @@ function App() {
   const [turnNumber, setTurnNumber] = useState(sessionStorage.getItem('turnNumber') ? JSON.parse(sessionStorage.getItem('turnNumber')) : 0)
   const [enemyTurnNumber, setEnemyTurnNumber] = useState(turnNumber)
 
-
   const [enemyBoardState, setEnemyBoardState] = useState(sessionStorage.getItem('enemyBoardState') ? JSON.parse(sessionStorage.getItem('enemyBoardState')) : generateBoard(true, true))
   // const [enemyName, setEnemyName] = useState(sessionStorage.getItem('enemyName'))
 
   const [character, setCharacter] = useState(false)
 
-  const [turn, setTurn] = useState(sessionStorage.getItem('turn') ? JSON.parse(sessionStorage.getItem('turn')) : true);
+  const [turn, setTurn] = useState(sessionStorage.getItem('turn') ? JSON.parse(sessionStorage.getItem('turn')) : true)
   const [orientation, setOrientation] = useState('h')
 
-  useEffect(() => {
-    if (Object.keys(cookies).length === 0) {
-      setCookie('user', { id: randomstring.generate(), state: 'matching', wins: 0, losses: 0 }
-      )
+  // websocket connection
+  // TODO: figure out how to deal with dependencies in `onmessage` without creating a new websocket every time
+  // I think there are some issues with having these inside an effect callback
+  // https://overreacted.io/a-complete-guide-to-useeffect/
+
+  this.componentDidMount = () => {
+    console.log('mounted!');
+  }
+
+  useEffect(function connect() {
+    socket.current = new WebSocket('ws://localhost:8080/ws')
+
+    // socket open
+    socket.current.onopen = () => {
+      console.log('Socket open.')
+      // TODO: enable finding games
     }
-  }, [cookies, setCookie])
-  useEffect(() => {
-    socket.current = new WebSocket('ws://localhost:8080/ws');
-    socket.current.onclose = () => {
-      console.log('connection closed')
+
+    // attempt reconnect after 1s
+    socket.current.onclose = (e) => {
+      console.log('Socket closed:', e.reason)
+      setTimeout(() => connect(), 1000)
     }
-  }, [])
-  useEffect(() => {
-    let messageListener = (event) => {
-      let message = JSON.parse(event.data)
+
+    // close on error
+    socket.current.onerror = (e) => {
+      console.error('Socket error:', e)
+      socket.current.close()
+    }
+
+    // websocket message handler
+    socket.current.onmessage = (e) => {
+      const message = JSON.parse(e.data)
       console.log(message)
+
       setTurnNumber(message.turnNumber)
       setEnemyTurnNumber(message.enemyTurnNumber)
+
       if (message.twoShots) {
         setLastShots(message.twoShots)
       }
       if (message.you) {
-        if (message.freeshotmiss || message.freeshotmiss === 0) {
-          setFreeShotMiss(message.freeshotmiss)
-        }
-        if (message.enemyfreeshotmiss || message.enemyfreeshotmiss === 0) {
-          setEnemyFreeShotMiss(message.enemyfreeshotmiss)
-        }
         if (message.freeshot) setTurn(true)
-        if (message.index) {
-          setMessages(prev => {
-            if (message.hit) return [...prev, `You fired at ${message.index} and it was a hit!`]
-            else return [...prev, `You fired at ${message.index} but it missed!`]
-          })
-        }
         if (message?.shipsSunk?.length > 0) {
           setMessages(prev => {
             return [...prev, `you have sunk their ${message.shipsSunk.join('and')}`]
           })
         }
+        if (typeof message.index !== 'object') {
+          setMessages(prev => {
+            if (message.hit) return [...prev, `You fired at ${message.index} and it was a hit!`]
+            else return [...prev, `You fired at ${message.index} but it missed!`]
+          })
+        }
         if (message.orange) {
+          // recursiveRemoveProtection(setBoardState)
           setBoardState(prev => {
-            if (!message.extrashot) {
-              let proSq = Object.values(prev).filter((item) => {
-                return item.state === 'protected'
-              }).map(item => item.id)
-              for (const sq of proSq) {
-                prev[sq].state = prev[sq].oldState
-                delete prev[sq].oldState
-              }
-            }
-            prev[message.index].oldState ||= prev[message.index].state
+            prev[message.index].oldState = prev[message.index].state
             prev[message.index].state = 'protected'
             return { ...prev }
           })
         }
-        if (message.bluffArray && message.retaliation) {
+        if (message.bluffArray) {
           setEnemyBoardState(prev => {
             for (const b of message.bluffArray) {
               prev[b].state = null
@@ -111,19 +114,11 @@ function App() {
             }
             return { ...prev }
           })
-        } else if (message.callbluff === 'success') {
-          setMessages(prev => {
-            return [...prev, `You called their bluff!`]
-          })
-        } else if (message.callbluff === 'failure') {
-          setMessages(prev => {
-            return [...prev, `they weren't bluffing!`]
-          })
         }
         if (message.array) {
           setMessages(prev => {
             return [...prev, `You fired a volley of shots, they hit ${message.shotresults.hit.join(', ')}!
-            and missed ${message.shotresults.missed.join(', ')}.`]
+          and missed ${message.shotresults.missed.join(', ')}.`]
           })
           setEnemyBoardState(prev => {
             for (const shot of message.shotresults.missed) {
@@ -140,6 +135,18 @@ function App() {
             prev[message.index].state = 'missed'
             return { ...prev }
           })
+        } else if (message.hit) {
+          if (Array.isArray(message.index)) {
+            setEnemyBoardState(prev => {
+              for (const shot of message.index)
+                prev[shot].state = 'hit'
+              return { ...prev }
+            })
+          } else
+            setEnemyBoardState(prev => {
+              prev[message.index].state = 'hit'
+              return { ...prev }
+            })
         } else if (message.hit) {
           if (Array.isArray(message.index)) {
             setEnemyBoardState(prev => {
@@ -242,26 +249,11 @@ function App() {
             }
             return { ...prev }
           })
-        }
-        if (message.missed) {
+        } else
           setBoardState(prev => {
-            prev[message.index].state = 'missed'
+            prev[message.index].state = 'hit'
             return { ...prev }
           })
-        }
-        if (message.hit) {
-          if (Array.isArray(message.index)) {
-            setBoardState(prev => {
-              for (const shot of message.index)
-                prev[shot].state = 'hit'
-              return { ...prev }
-            })
-          } else
-            setBoardState(prev => {
-              prev[message.index].state = 'hit'
-              return { ...prev }
-            })
-        }
       }
       if (message.matched) {
         setMessages(prev => {
@@ -285,19 +277,13 @@ function App() {
         console.log('got the boats')
       }
     }
-    socket.current.addEventListener('message', messageListener)
-    return () => {
-      socket.current.removeEventListener('message', messageListener)
-    }
-  }, [bluffing, setLastShots, setBluffing])
-
-
+  }, [])
 
   return (
     <div className={styles.app}>
 
       <button onClick={() => {
-        removeCookie('user')
+        cookies.remove('user')
         setGameProgress('preplacement')
       }}>remove cookie</button>
       {/* {(socket?.readyState !== undefined && gameProgress === 'preplacement') && <div>connected</div>} */}
@@ -313,13 +299,13 @@ function App() {
 
           <Board player board={boardState} character={character} socket={socket.current}
             boatNames={boatNames} setBoatNames={setBoatNames}
-            cookies={cookies} setCookie={setCookie}
+            cookies={cookies} setCookie={cookies.set}
             boardState={boardState} setBoardState={setBoardState}
             enemyBoardState={enemyBoardState}
             orientation={orientation} gameProgress={gameProgress} setGameProgress={setGameProgress}
           />
           <EnemyBoard character={character} board={boardState} enemyBoardState={enemyBoardState} socket={socket}
-            cookies={cookies} setCookie={setCookie} setEnemyBoardState={setEnemyBoardState}
+            cookies={cookies} setCookie={cookies.set} setEnemyBoardState={setEnemyBoardState}
             boardState={boardState} turn={turn} setTurn={setTurn}
             // enemyName={enemyName} 
             setBoardState={setBoardState} gameProgress={gameProgress} setGameProgress={setGameProgress}
@@ -353,7 +339,7 @@ function App() {
               socket.current.send(JSON.stringify({ ...cookies.user, character }))
             }}>find game</button> */}
             <Customization character={character} setCharacter={setCharacter} boatNames={boatNames}
-              setBoatNames={setBoatNames} setCookie={setCookie} cookies={cookies}
+              setBoatNames={setBoatNames} setCookie={cookies.set} cookies={cookies}
               socket={socket} />
           </> :
           <></>
@@ -361,11 +347,10 @@ function App() {
         }
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
-
+export default App
 
 /* <div className={styles.title}>WELCOME TO BATTLESHIP</div>
 <div className={styles.boardcontainer}>
