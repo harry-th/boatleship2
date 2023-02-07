4e09fbcd66a0cb0215d4902814ec4652081ebe96
Johann von Tiesenhausen <jtiesenhausen@macleanengineering.com>
2023-02-07 17:39:22 -0500

    cleared client issues

--- src/server/client.js
+++ src/server/client.js
@@ -6,57 +6,27 @@
  * state modified by server messages to be at the App level.
 */
 
-function Client(url, protocols) {
-  this.socket = null;
-  this.listeners = {};  // { type: [ listener, ... ] }
+class Client extends EventEmitter {
+  constructor(url, protocols) {
+    this.url = url;
+    this.protocols = protocols;
+    this.socket = null;
+    this.reconnect();
+  }
 
   // send a message to the server
-  this.send = (type, data) => {
+  send(type, data) {
     this.socket.send(JSON.stringify({ type, data }));
-  };
+  }
 
   // close the underlying websocket
-  this.close = () => {
+  close() {
     this.socket.close();
-  };
-
-  // emit a message to relevant event listeners
-  this.emit = (type, data) => {
-    if (this.listeners[type]) {
-      for (const listener of this.listeners[type]) {
-        listener(data);
-      }
-    }
-  };
-
-  // add a unique event listener
-  this.addListener = (type, listener) => {
-    if (!this.listeners[type]) {
-      this.listeners[type] = [];
-    }
-    const arr = this.listeners[type];
-    const i = arr.indexOf(listener);
-
-    if (i === -1) {
-      console.log('added', listener);
-      arr.push(listener);
-    }
-  };
-
-  // remove an event listener
-  this.removeListener = (type, listener) => {
-    const arr = this.listeners[type];
-    const i = arr?.indexOf(listener);
-
-    if (i !== undefined && i !== -1) {
-      console.log('removed', listener);
-      arr.splice(i, 1);
-    }
-  };
+  }
 
   // create a new websocket connection
-  this.reconnect = () => {
-    this.socket = new WebSocket(url, protocols);
+  reconnect() {
+    this.socket = new WebSocket(this.url, this.protocols);
 
     // generic open
     this.socket.addEventListener('open', (e) => {
@@ -84,9 +54,7 @@ function Client(url, protocols) {
       this.emit('error', null);
       this.socket.close();
     });
-  };
-
-  this.reconnect();
-};
+  }
+}
 
 export default Client;
